62fc91057e1493c5542e5df4891f42147711c33e
JuaneDev <121892094+juaneprograming@users.noreply.github.com>
2024-04-15 15:41:11 +0000

    fentes diferentes para componentes en concreto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { montserrat } from './ui/fonts';
 import './ui/global.css'
 export default function RootLayout({
   children,
@@ -6,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className= { `${montserrat.className} antialiased `}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Montserrat, Poppins } from 'next/font/google'
+
+export const montserrat = Montserrat({ subsets: ["latin"] })
+
+export const poppins = Poppins({ weight: ['400', '600' ], subsets: ["latin"]})
